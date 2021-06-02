b221be23f3cc27677509e785116802c09ff4e446
Edson Shideki Kokado <eskokado@gmail.com>
2021-06-02 15:46:18 -0300

    Initial commit

---
+++ src/main/java/com/esk/desafio/dio/EskPeopleApi/EskPeopleApiApplication.java
@@ -0,0 +1,13 @@
+package com.esk.desafio.dio.EskPeopleApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskPeopleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskPeopleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esk/desafio/dio/EskPeopleApi/EskPeopleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esk.desafio.dio.EskPeopleApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskPeopleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
