18e01d2fc5f09a5e1def79f526a4371c631eb4e4
Edson Shideki Kokado <eskokado@gmail.com>
2021-06-09 06:00:29 -0300

    Add Query with multiple params

--- src/main/java/com/esk/desafio/dio/EskPeopleApi/resources/PostResource.java
+++ src/main/java/com/esk/desafio/dio/EskPeopleApi/resources/PostResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -70,6 +71,18 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByBody(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> list = postService.findByBody(text);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
         return ResponseEntity.ok(list);
     }
 }
--- src/main/java/com/esk/desafio/dio/EskPeopleApi/services/PostService.java
+++ src/main/java/com/esk/desafio/dio/EskPeopleApi/services/PostService.java
@@ -10,6 +10,7 @@ import com.esk.desafio.dio.EskPeopleApi.services.exceptions.ObjectNotFoundExcept
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -49,6 +50,11 @@ public class PostService {
 
     public List<Post> findByBody(String text) {
         return postRepository.findByBody(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 
     private void updateData(Post objNew, Post obj) {
